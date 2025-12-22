814aa92ccbf92137c1c375ebce8c92cfa1b0624e
Tristan Louie Pineda <tlcpineda.projects@gmail.com>
2025-12-22 21:14:44 +0800

    Finalise Module 6.

--- mod_06.py
+++ mod_06.py
@@ -1,5 +1,6 @@
 """
-
+Compile PSD files contained in the selected folder into a single PDF file.
+PDF filename is parsed from the parent directory of the files.
 """
 import os
 from operator import not_
@@ -19,19 +20,20 @@ lang_dict = {
     'kh': 'Khmer',
     'hi': 'Hindi'
 }   # FUTURE To be harmonised with work title codes:  etc "2025-Q4-KH-B5-34", "2025-Q4-HI-B2-12", and title log.
+filename_pattern = '{TitleName_vol[3]_chap[4]_pg[3] pg[2]}.psd' or '{TitleName_vol[3]_chap[4]_pg[3] pg[2]}X.psd'
 
 def compile_to_pdf(input_folder):
-    # Get and sort files from folder
-    files = [f for f in os.listdir(input_folder) if (' ' in f and os.path.splitext(f)[1].lower() == '.psd')]
-
-    try:
-        files.sort(key=get_sort_number)
-    except ValueError:
-        files.sort()
+    # Get and sort PSD files from folder; only files that follow filename pattern.
+    files = filter_files(input_folder)
 
     if not files:
-        print("No PSD files found.")
+        print("No PSD files fit to be compiled.")
         return
+
+    try:
+        files.sort(key=get_pg_num)
+    except ValueError:
+        files.sort()
 
     # Initialise the generator
     # Slice [1:]; first image is handled by the save() call, as anchor
@@ -67,16 +69,33 @@ def compile_to_pdf(input_folder):
         )
 
 
-def get_sort_number(filename: str):
+def filter_files(folder: str) -> list:
+    """"
+    Filter files that follow the filename pattern, with the last two/three digits as the page markers.
+    :param folder: The parent folder of the PSD files
+    :return filtered_files: The list of filtered PSD files to be compiled
     """
+    filtered_files = []
 
-    :param filename:
-    :return:
+    for f  in os.listdir(folder):
+        # Check if filename follows filename pattern.
+        if ' ' in f:# Append file to return list if page_marker exists.
+            page = get_pg_num(f)
+            if page: filtered_files.append(f)
+
+    return filtered_files
+
+
+def get_pg_num(filename: str) -> int:
     """
-    name_only = filename.lower().replace('.psd', '')
-    num_str = "".join([char for char in name_only if char.isdigit()])
+    Extract the page number from the filename.
+    :param filename: The filename of the PSD file.
+    :return: The page number
+    """
+    basename = os.path.splitext(filename)[0]
+    pg_str = ''.join([char for char in basename.split(' ')[1] if char.isdigit()])
 
-    return int(num_str) if num_str else filename
+    return int(pg_str) if pg_str else 999 # Return an absurdly large number if pg_str is null string.
 
 
 def image_generator(folder: str, files: list) -> Iterator:
@@ -107,54 +126,52 @@ def image_generator(folder: str, files: list) -> Iterator:
 
 def gen_out_filepath(folder_path: str) -> str:
     """
-
+    Generate the complete path and filename to be used by the PDF file.
     :param folder_path: The path pointing to the parent folder of the PSD folder.
     :return: The PDF path where the images will be converted to
     """
     parent = os.path.dirname(folder_path)
-    title_folder, ch_folder = parent.split("\\")[-2:]
-
+    title_folder, ch_folder = parent.split("/")[-2:]
     title_split = title_folder.split(" ")
     lang_iso = title_split[0].split('-')[2].lower()
     ch_num = ch_folder.replace('CH', '')
-    title = ' '.join(title_split[1:]).title()
-    pdf_name = f"{title}_{lang_dict[lang_iso]} CH_{ch_num}_For TP Check.pdf"
+    title = ' '.join(title_split[1:]) # Assume that the title is already properly capitalised.
+    pdf_name = f"{title}_{lang_dict[lang_iso]} CH {ch_num}_For TP Check.pdf"
+    out_filepath = os.path.join(parent, pdf_name)
 
-    return os.path.join(parent, pdf_name)
+    if os.path.exists(out_filepath):
+        out_filepath = os.path.join(parent, f"COPY {pdf_name}")
+
+    return out_filepath
 
 
 if __name__ == "__main__":
-    # module_06_psd_to_pdf_streaming("./chapter_folder", "QA_Reference_72dpi.pdf")
-    # gen_out_filepath(psd_folder)
+    welcome_sequence([
+        mod_name,
+        f"ver {mod_ver} {date}",
+        email
+    ])
 
-    while False:
-        welcome_sequence([
-            mod_name,
-            f"ver {mod_ver} {date}",
-            email
-        ])
+    print(input("\n>>> Press enter to continue ..."))
 
-        print(input("\n>>> Press enter to continue ..."))
+    confirm_exit = False
 
-        confirm_exit = False
+    while not confirm_exit:
+        print(">>> Select PSD folder ...")
 
-        while not confirm_exit:
-            print(">>> Select a PDF file to scrape ...")
+        path = identify_path("folder")
 
-            path = identify_path("folder")
+        if path:
+            compile_to_pdf(path)
+        else:
+            print("\n<=> No folder selected.")
 
-            if path:
-                # get psd_folder
-                compile_to_pdf(path)
-            else:
-                print("\n<=> No folder selected.")
+        if continue_sequence() == "X":
+            confirm_exit = True
 
-            if continue_sequence() == "X":
-                confirm_exit = True
+            print("\n<=> Closing down ...")
+        else:
+            confirm_exit = False
 
-                print("\n<=> Closing down ...")
-            else:
-                confirm_exit = False
-
-                print("\n<=> Restarting ...")
+            print("\n<=> Restarting ...")
 
