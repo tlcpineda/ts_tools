26cdfc21255b7fe4b063134aed0a8226711a2936
Tristan Louie Pineda <tlcpineda.projects@gmail.com>
2026-01-15 02:13:14 +0800

    Initial set up for Module 6.

--- TS Tools.py
+++ TS Tools.py
@@ -2,7 +2,7 @@
 A compilation of modules to assist in local processes during typesetting.
 """
 
-from lib import welcome_sequence, hor_bar
+from lib import hor_bar, welcome_sequence
 from mod_01 import get_translations
 from mod_03 import process_rev_file
 from mod_04 import rename_files
@@ -14,31 +14,35 @@ app_ver = "1.00.01"
 date = "28 Dec 2025"
 email = "tlcpineda.projects@gmail.com"
 options = [
+    # {
+    #     "menu": "[F]etch files from Drive",
+    #     "shortkey": "F",
+    #     "func": fetch_from_drive,
+    # },
     {
-        'menu': '[S]crape translations',
-        'shortkey': 'S',
-        'func': get_translations,
+        "menu": "[S]crape translations",
+        "shortkey": "S",
+        "func": get_translations,
     },
     {
-        'menu': '[M]ark files for revision',
-        'shortkey': 'M',
-        'func': process_rev_file,
+        "menu": "[M]ark files for revision",
+        "shortkey": "M",
+        "func": process_rev_file,
     },
     {
-        'menu': '[R]ename files',
-        'shortkey': 'R',
-        'func': rename_files,
+        "menu": "[R]ename files",
+        "shortkey": "R",
+        "func": rename_files,
     },
-    {
-        'menu': '[C]ompile PSD',
-        'shortkey': 'C',
-        'func': compile_to_pdf
-    },
-    {
-        'menu': "E[X]it and close window",
-        'shortkey': 'X'
-    }
+    {"menu": "[C]ompile PSD", "shortkey": "C", "func": compile_to_pdf},
+    # {
+    #     "menu": "[U]pload files to Drive",
+    #     "shortkey": "U",
+    #     "func": upload_to_drive,
+    # },
+    {"menu": "E[X]it and close window", "shortkey": "X"},
 ]
+
 
 def display_menu():
     print("")
@@ -46,7 +50,7 @@ def display_menu():
     hor_bar(60, "MENU")
 
     for option in options:
-        print(f"{" ":5}{option['menu']}")
+        print(f"{' ':5}{option['menu']}")
 
     hor_bar(60)
 
@@ -54,11 +58,13 @@ def display_menu():
 
 
 if __name__ == "__main__":
-    welcome_sequence([
-        app_name,
-        f"ver {app_ver} {date}",
-        email,
-    ])
+    welcome_sequence(
+        [
+            app_name,
+            f"ver {app_ver} {date}",
+            email,
+        ]
+    )
 
     confirm_exit = False
 
@@ -69,18 +75,20 @@ if __name__ == "__main__":
         while not user_input:
             resp = input(">>> ").upper()
 
-            if resp in [option['shortkey'] for option in options]: user_input = resp
+            if resp in [option["shortkey"] for option in options]:
+                user_input = resp
 
-        if user_input == 'X':
+        if user_input == "X":
             hor_bar(60, "CLOSING DOWN ...")
-            confirm_exit=True
+            confirm_exit = True
         else:
             print("")
 
-            selected_option = [option for option in options if option['shortkey'] == user_input][0]
-            func_selected = selected_option['func'] # requires input_path
+            selected_option = [
+                option for option in options if option["shortkey"] == user_input
+            ][0]
+            func_selected = selected_option["func"]  # requires input_path
 
             hor_bar(60, f"RUNNING : {func_selected.__name__}()")
             func_selected()
             hor_bar(60, f"COMPLETE : {func_selected.__name__}()")
-
